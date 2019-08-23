85c9f28e6b76140a76d717c333c7173b82ecf825
Alex Bepple <alex@bepple.de>
2019-08-23 23:23:48 +0200

    . distinguish public and private names in canonical def

--- src/foo.test.js
+++ src/foo.test.js
@@ -1,32 +1,38 @@
 import * as r from 'ramda'
 import * as __ from 'hamjest'
 
+const canonizePropDef = r.pipe(
+  r.when(r.is(String))((def) => ({ name: def })),
+  (withName) =>
+    r.pipe(
+      () => ({ publicName: withName.name, privateName: withName.name }),
+      r.merge(withName),
+      r.dissoc('name')
+    )()
+)
+
 const getGetterEnhancers = r.pipe(
-  r.map((x) => ({ [x.name]: x.get })),
+  r.map((x) => ({ [x.publicName]: x.get })),
   r.mergeAll,
   r.reject(r.isNil)
 )
 
 const createType = (propDefs) => {
-  const normalizedPropDefs = r.pipe(
-    () => propDefs,
-    r.map(r.unless(r.is(Object))(r.objOf('name')))
-  )()
-  const _props = r.pipe(
-    () => normalizedPropDefs,
-    r.map(r.prop('name')),
-    r.map((x) => ({ [x]: x })),
+  const canonicalPropDefs = r.map(canonizePropDef)(propDefs)
+  const nameMap = r.pipe(
+    () => canonicalPropDefs,
+    r.map((x) => ({ [x.publicName]: x.privateName })),
     r.mergeAll
   )()
-  const rawGet = r.map(r.prop)(_props)
+  const rawGet = r.map(r.prop)(nameMap)
   const get = r.mergeAll([
     rawGet,
     r.pipe(
-      () => normalizedPropDefs,
+      () => canonicalPropDefs,
       getGetterEnhancers,
-      r.mapObjIndexed((fn, prop) =>
+      r.mapObjIndexed((fn, publicName) =>
         r.pipe(
-          rawGet[prop],
+          rawGet[publicName],
           fn
         )
       )
@@ -34,10 +40,12 @@ const createType = (propDefs) => {
   ])
 
   return {
-    props: r.map(r.always)(_props),
+    props: r.map(r.always)(nameMap),
     get,
-    pick: r.map((prop) => (obj) => ({ [prop]: get[prop](obj) }))(_props),
-    set: r.map(r.assoc)(_props)
+    pick: r.map((privateName) => (obj) => ({
+      [privateName]: get[privateName](obj)
+    }))(nameMap),
+    set: r.map(r.assoc)(nameMap)
   }
 }
 
@@ -64,14 +72,6 @@ describe('KISS type', () => {
     })
   })
 
-  describe('with prop defined in extended form', () => {
-    const type = createType([{ name: 'prop' }])
-
-    it('exposes all the same facilities as when prop is defined in basic form', () => {
-      __.assertThat(type.get.prop({ prop: 42 }), __.is(42))
-    })
-  })
-
   describe('with getter enhancer', () => {
     const type = createType([{ name: 'prop', get: r.defaultTo(0) }])
 
@@ -80,6 +80,37 @@ describe('KISS type', () => {
     })
     it('uses enhancer for #pick', () => {
       __.assertThat(type.pick.prop({}), __.is({ prop: 0 }))
+    })
+  })
+
+  describe('Canonical prop definition', () => {
+    describe('can be derived from basic prop definition (just a string)', () => {
+      it('derives public and private names', () => {
+        __.assertThat(
+          canonizePropDef('prop'),
+          __.hasProperties({
+            publicName: 'prop',
+            privateName: 'prop'
+          })
+        )
+      })
+    })
+    describe('can be derived from temporary extended definition', () => {
+      it('derives public and private names', () => {
+        __.assertThat(
+          canonizePropDef({ name: 'prop' }),
+          __.allOf(
+            __.hasProperties({ publicName: 'prop', privateName: 'prop' }),
+            __.not(__.hasProperty('name'))
+          )
+        )
+      })
+      it('preserves other props', () => {
+        __.assertThat(
+          canonizePropDef({ name: 'foo', get: 'bar' }),
+          __.hasProperty('get')
+        )
+      })
     })
   })
 })
