46bbc0a2ac97254d233b52cdf95d71b701ac5597
Alex Bepple <alex@bepple.de>
2019-08-23 18:11:16 +0200

    $ prop names

--- src/foo.test.js
+++ src/foo.test.js
@@ -2,16 +2,19 @@ import * as r from 'ramda'
 import * as __ from 'hamjest'
 
 const createType = (props) => {
+  const _props = r.mergeAll(r.map((x) => r.objOf(x, x))(props))
   return {
-    get: r.mergeAll(
-      r.map((prop) => r.objOf(r.identity(prop), r.prop(prop)))(props)
-    )
+    props: r.map(r.always)(_props),
+    get: r.map(r.prop)(_props)
   }
 }
 
 describe('KISS type', () => {
   describe('in its most basic form', () => {
     const type = createType(['prop'])
+    it('exposes property names', () => {
+      __.assertThat(type.props.prop(), __.is('prop'))
+    })
     it('gives property access', () => {
       __.assertThat(type.get.prop({ prop: 42 }), __.is(42))
     })
