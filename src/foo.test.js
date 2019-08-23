3383b218bcbc7fe7385db8b88ce48b55e8791604
Alex Bepple <alex@bepple.de>
2019-08-23 18:07:00 +0200

    $ get

--- src/foo.test.js
+++ src/foo.test.js
@@ -1,7 +1,19 @@
-import {assertThat, is} from 'hamjest'
+import * as r from 'ramda'
+import * as __ from 'hamjest'
 
-describe('foo', () => {
-  it('bar', () => {
-    assertThat(1+1, is(3))
+const createType = (props) => {
+  return {
+    get: r.mergeAll(
+      r.map((prop) => r.objOf(r.identity(prop), r.prop(prop)))(props)
+    )
+  }
+}
+
+describe('KISS type', () => {
+  describe('in its most basic form', () => {
+    const type = createType(['prop'])
+    it('gives property access', () => {
+      __.assertThat(type.get.prop({ prop: 42 }), __.is(42))
+    })
   })
 })
